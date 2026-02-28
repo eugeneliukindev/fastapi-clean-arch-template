6292f98f09eca46f662978801c2952935352190b
eugene <eugeneliukin.dev@gmail.com>
2026-03-01 01:32:44 +0300

    StringConstraints for user create

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,15 +1,16 @@
 from datetime import datetime
+from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, StringConstraints
 
 from src.core.enums import UserStatus
 from src.schemas.base import PartialModel
 
 
 class UserCreate(BaseModel):
-    first_name: str
-    last_name: str
+    first_name: Annotated[str, StringConstraints(min_length=4, max_length=35)]
+    last_name: Annotated[str, StringConstraints(min_length=4, max_length=35)]
     status: UserStatus = UserStatus.ACTIVE
 
 
