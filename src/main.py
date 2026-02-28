1d6c325f1ac3bf29f3b017fdc9b425e4d7d31ebd
eugene <eugeneliukin.dev@gmail.com>
2026-03-01 01:13:08 +0300

    add main.py

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from src.api import router as api_router
+from src.core.database import db_manager
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    yield
+    await db_manager.engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
